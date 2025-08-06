918e9ec84d628e704e322aa9241ff7ab0c9c9ab2
I001962 <i001962@gmail.com>
2025-08-06 15:51:24 -0700

    feat: allow paying with eth or usdc

--- src/app/[...slug]/components/PayCard/PayInput.tsx
+++ src/app/[...slug]/components/PayCard/PayInput.tsx
@@ -7,43 +7,56 @@ export interface PayInputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   withPayOnSelect?: boolean;
-  currency?: string;
+  currency?: React.ReactNode;
   inputClassName?: string;
 }
 
 const PayInput = React.forwardRef<HTMLInputElement, PayInputProps>(
-  ({ className, inputClassName, label, type, currency, withPayOnSelect, ...props }, ref) => {
+  (
+    {
+      className,
+      inputClassName,
+      label,
+      type,
+      currency,
+      withPayOnSelect,
+      ...props
+    },
+    ref,
+  ) => {
     return (
       <div
         className={cn(
-          "flex h-30 px-4 py-4 w-full items-center justify-between shadow-sm focus-within:ring-1 focus-within:ring-inset focus-within:ring-zinc-500 bg-zinc-100",
-          className
+          "h-30 flex w-full items-center justify-between bg-zinc-100 px-4 py-4 shadow-sm focus-within:ring-1 focus-within:ring-inset focus-within:ring-zinc-500",
+          className,
         )}
       >
         <div className="flex flex-col">
           <div className="flex flex-row gap-1">
-            <label className="text-md text-black-700">
-              {label}
-            </label>
-            {withPayOnSelect && (
-              <PayOnSelect />
-            )}
+            <label className="text-md text-black-700">{label}</label>
+            {withPayOnSelect && <PayOnSelect />}
           </div>
           <input
             type={type}
             className={cn(
-              "border-0 bg-transparent pl-0 pr-3 pt-1 pb-0 text-zinc-900 text-2xl w-full placeholder:text-zinc-400 focus:ring-0 sm:leading-6",
-              inputClassName
+              "w-full border-0 bg-transparent pb-0 pl-0 pr-3 pt-1 text-2xl text-zinc-900 placeholder:text-zinc-400 focus:ring-0 sm:leading-6",
+              inputClassName,
             )}
             ref={ref}
             placeholder="0.00"
             {...props}
           />
         </div>
-        <span className="text-right select-none text-lg">{currency}</span>
+        {currency ? (
+          typeof currency === "string" ? (
+            <span className="select-none text-right text-lg">{currency}</span>
+          ) : (
+            <div className="text-right text-lg">{currency}</div>
+          )
+        ) : null}
       </div>
     );
-  }
+  },
 );
 PayInput.displayName = "PayInput";
 
